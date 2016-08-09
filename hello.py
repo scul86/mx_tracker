060aef660ea6d8991492c2d853ef956d2bdccd07
Kyle <kyle.sims@comcast.net>
2016-08-09 00:25:22 -0600

    update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 #!venv/bin/python3
 
 from flask import Flask
+from flask_sslify import SSLify
 
 app = Flask(__name__)
+#sslify = SSLify(app)
 
 @app.route('/')
 def index():
     return '<h1> Hello World</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}</h1>'.format(name)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
