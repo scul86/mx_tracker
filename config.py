a5274cc064a2f6b8964acfa8340e4e4a388dba21
Kyle <kyle.sims@comcast.net>
2016-08-11 17:33:29 -0600

    update models and test

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ SECRET_KEY = 'you-will-never-guess'
 '''
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 # WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
 # email server
--- test.py
+++ test.py
@@ -1,11 +1,39 @@
 #!venv/bin/python3
 
-from app import models, db
+import os
+import unittest
+import bcrypt
 
-v = models.Vehicle(name='Ranger')
+from config import basedir
+from app import app, db
+from app.models import Vehicle, GasStop
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_passwd(self):
+        passwd = bcrypt.hashpw(b'Testing', bcrypt.gensalt())
+        v = Vehicle(name='Ranger', passwd=passwd)
+        assert v.passwd == bcrypt.hashpw(b'Testing', passwd)
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''
+v = Vehicle(name='Ranger')
 print('Prior to add(v)')
 db.session.add(v)
 print('Prior to commit()')
 db.session.commit()
 
 input('Press Enter')
+'''
