f3eca40c1873c88dd6217ef7f15cf686e6bc22f7
Kyle <kyle.sims@comcast.net>
2016-08-11 19:25:14 -0600

    update test.py

--- test.py
+++ test.py
@@ -22,27 +22,22 @@ class TestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_name(self):
-        v = Vehicle(name='Testing')
-        assert v.name == 'Testing'
-
-    def test_passwd(self):
-        passwd = 'Testing'.encode('utf-8')
+    def test_vehicle(self):
+        passwd = 'Testing Password'.encode('utf-8')
         passwd = bcrypt.hashpw(passwd, bcrypt.gensalt())
-        v = Vehicle(passwd=passwd)
-        assert v.passwd == bcrypt.hashpw(b'Testing', passwd)
-
-    def test_mileage(self):
-        v = Vehicle(mileage=10.90)
-        assert v.mileage == 10.9
-
-    def test_last_update(self):
         time = datetime.utcnow()
-        v = Vehicle(last_updated=time)
+        v = Vehicle(name='Ranger',
+                    passwd=passwd,
+                    mileage=10.90,
+                    last_updated=time)
+
+        assert v.name == 'Ranger'
+        assert v.passwd == bcrypt.hashpw(b'Testing Password', passwd)
+        assert v.mileage == 10.9
         assert v.last_updated == time
 
     def test_gas_stop(self):
-        v = Vehicle()
+        v = Vehicle(name='Ranger')
         GasStop(gallons=10.9,
                 price=1.25,
                 trip=123.45,
@@ -53,6 +48,7 @@ class TestCase(unittest.TestCase):
         assert stop.price == 1.25
         assert stop.trip == 123.45
         assert stop.mpg == 123.45/10.9
+        assert stop.vehicle.name == 'Ranger'
 
 if __name__ == '__main__':
     unittest.main()
