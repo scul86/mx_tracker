01c127cf9a687725f633e38db163fa9fd665f7bf
Kyle <kyle.sims@comcast.net>
2016-08-11 19:16:16 -0600

    update models and test

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Vehicle(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     passwd = db.Column(db.String(128))
     mileage = db.Column(db.DECIMAL, index=True)
-    gas_stop = db.relationship('GasStop', backref='veh', lazy='dynamic')
+    gas_stop = db.relationship('GasStop', backref='vehicle', lazy='dynamic')
     # maint = db.relationship('Maintenance', backref='vehicle', lazy='dynamic')
     # about_me = db.Column(db.String(140))
     last_updated = db.Column(db.DateTime)
--- test.py
+++ test.py
@@ -13,7 +13,8 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = \
+            'sqlite:///' + os.path.join(basedir, 'test.db')
         self.app = app.test_client()
         db.create_all()
 
@@ -40,15 +41,18 @@ class TestCase(unittest.TestCase):
         v = Vehicle(last_updated=time)
         assert v.last_updated == time
 
+    def test_gas_stop(self):
+        v = Vehicle()
+        GasStop(gallons=10.9,
+                price=1.25,
+                trip=123.45,
+                vehicle=v)
+        stop = v.gas_stop.all()[0]
+        stop.mpg = stop.trip/stop.gallons
+        assert stop.gallons == 10.9
+        assert stop.price == 1.25
+        assert stop.trip == 123.45
+        assert stop.mpg == 123.45/10.9
+
 if __name__ == '__main__':
     unittest.main()
-
-'''
-v = Vehicle(name='Ranger')
-print('Prior to add(v)')
-db.session.add(v)
-print('Prior to commit()')
-db.session.commit()
-
-input('Press Enter')
-'''
