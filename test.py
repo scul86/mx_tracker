adef15de2cb6bebf3028f4dc15739ac496fda6e7
Kyle <kyle.sims@comcast.net>
2016-08-11 22:23:57 -0600

    update test

--- test.py
+++ test.py
@@ -46,16 +46,16 @@ class TestCase(unittest.TestCase):
 
     def test_gas_stop(self):
         v = Vehicle(name='Ranger')
-        GasStop(gallons=10.9,
+        GasStop(gallons=10.5,
                 price=1.25,
                 trip=123.45,
                 vehicle=v)
         stop = v.gas_stop.all()[0]
         stop.mpg = stop.trip/stop.gallons
-        assert stop.gallons == 10.9
+        assert stop.gallons == 10.5
         assert stop.price == 1.25
         assert stop.trip == 123.45
-        assert stop.mpg == 123.45/10.9
+        assert stop.mpg == 123.45/10.5
         assert stop.vehicle.name == 'Ranger'
 
 if __name__ == '__main__':
