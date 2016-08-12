01b21d917cf528d7e732481e66e0a70a1b48cdf4
Kyle <kyle.sims@comcast.net>
2016-08-11 22:25:16 -0600

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .auth import auth as auth_blueprint
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.register_blueprint(auth_blueprint)
 db = SQLAlchemy(app)
 
 from app import views, models
