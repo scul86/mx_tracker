524fcc208776706552947c84ef1c0074641031ac
Kyle <kyle.sims@comcast.net>
2016-08-13 15:07:40 -0600

    modify views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app, db
 from .models import Vehicle, GasStop
 from .forms import AddGasStopForm
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, abort
 from flask_login import login_required
 
 
@@ -14,7 +14,11 @@ def index():
 @app.route('/vehicle/<name>')
 @login_required
 def vehicle(name):
-    return '<h1>Hello, {}</h1>'.format(name)
+    v = Vehicle.query.filter_by(name=name).first()
+    if not v:
+        abort(404)
+    stops = v.gas_stop.all()
+    return render_template('vehicle.html', vehicle=v, stops=stops)
 
 
 @app.route('/add_gas_stop', methods=['GET', 'POST'])
@@ -23,17 +27,20 @@ def add_gas_stop():
     form = AddGasStopForm()
     if form.validate_on_submit():
         v = Vehicle.query.filter_by(name=form.vehicle.data).first()
-        g = GasStop()
-        g.gallons = form.gallons.data
-        g.price = form.price.data
-        g.trip = form.trip.data
-        g.mpg = g.trip / g.gallons
-        g.location = form.location.data
-        g.vehicle_id = v
-        v.add_mileage(g.trip)
-        db.session.add(g)
-        db.session.commit()
-        flash('Record added')
-        # TODO: fix the name arg in below line
-        return redirect(request.args.get('next') or url_for('vehicle', name=v.name))
+        if v is not None and v.is_authenticated:
+            print('Authenticated {}'.format(v))
+            g = GasStop()
+            g.gallons = form.gallons.data
+            g.price = form.price.data
+            g.trip = form.trip.data
+            g.mpg = g.trip / g.gallons
+            g.location = form.location.data
+            g.vehicle_id = v
+            v.add_mileage(g.trip)
+            db.session.add(g)
+            # db.session.commit()
+            flash('Record added')
+            return redirect(request.args.get('next') or url_for('vehicle', name=v.name))
+        flash('Please login')
+        return  redirect(url_for('auth.login'))
     return render_template('add_gas_stop.html', form=form)
