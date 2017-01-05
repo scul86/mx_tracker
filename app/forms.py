b14d8b441cb83a5a3dde7dcd7ac510bfd015e12d
scul86 <scul86@users.noreply.github.com>
2017-01-04 23:50:12 -0700

    Change type of field
    Update numerical fields to use number touchpad on mobile

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
-from wtforms import BooleanField, StringField, DecimalField, \
+from wtforms.fields.html5 import DecimalField
+from wtforms import BooleanField, StringField, \
     SubmitField, DateTimeField, SelectField
 from wtforms.validators import DataRequired, Length, Optional
 from config import DATE_TIME_FORMAT
